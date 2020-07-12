589e081dc6b91c39c2b1973802c9f5fadce33bc5
TKSomar <tksomar@gmail.com>
2020-07-12 17:07:00 -0500

    initial commit

---
+++ src/Components/Login/Login.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import './Login.scss';
+
+function Login() {
+    const [inputs, setInputs] = useState({
+        username: '',
+        password: ''
+    });
+    const { username, password } = inputs;
+    const [submitted, setSubmitted] = useState(false);
+    const [] = useState(false);
+
+    return (
+        <div></div>
+    )
+}
--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,16 +1,74 @@
 import React, { useState } from 'react';
 import './Nav.scss';
-import { Link } from 'react-router-dom';
 
 function Nav ({ background, hoverBackground, linkColor, logo, navLinks }) {
-    // const { subOpen, setSubOpen } = useState(false)
-    // const { accountOpen, setAccountOpen } = useState(false)
+    // const [ subforumMenuOpen, setSubforumMenuOpen ] = useState(false)
+    // const [ userMenuOpen, setUserMenuOpen ] = useState(false)
+    const [ loggedIn, setLoggedIn ] = useState(false)
 
     console.log(background, hoverBackground, linkColor, logo, navLinks)
 
-    return (
-        <nav className="Nav" style={( background )}>
 
+
+    return (
+        <nav className="Nav">
+
+            { loggedIn ? 
+
+            <div className="user-nav">
+
+                <div className="logo-sub-search-cont">
+
+                    <div className="logo-container">
+                        <img src={logo} alt=""/>
+                    </div>
+
+                    <div className="subforum-menu-cont">
+                
+                    </div>
+
+                    <div className="user-search-cont">
+
+                    </div>
+
+                </div>
+                
+                <div className="user-toolbar">
+
+                    <div className="icons-cont">
+
+                    </div>
+
+                    <div className="user-menu-cont">
+
+                    </div>
+
+                </div>
+
+                
+
+            </div>
+
+                :
+
+            <div className="no-user-toolbar">
+
+                <div className="logo-search-cont">
+
+                    <div className="logo-container">
+                        <img src={logo} alt=""/>
+                    </div>
+
+                </div>
+
+                <div className="login-signup-btns-cont">
+                    <button>Log In</button>
+                    <button>Signup</button>
+                </div>
+
+            </div>
+                
+            }
 
         </nav>
     )
