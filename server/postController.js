6b005a315cd73ae28511e79ece757f2910e51862
chasedurrett <chase.durrett80@gmail.com>
2020-07-13 17:50:32 -0500

    finished home-dash endpoints and user PUT endpoint

--- server/postController.js
+++ server/postController.js
@@ -1,58 +1,76 @@
 //includes comments
-const moment = require('moment');
+const moment = require("moment");
 
 module.exports = {
-    getAllSubforumPosts: async (req, res) => {
-        const db = req.app.get('db')
+  getAllSubforumPosts: async (req, res) => {
+    const db = req.app.get("db");
 
-        const {subforumId} = req.params
+    const { subforumId } = req.params;
 
-        let posts = await db.post.get_all_posts(subforumId)
+    let posts = await db.post.get_all_posts(subforumId);
 
-        res.status(200).send(posts)
-    },
+    res.status(200).send(posts);
+  },
 
-    createSubforumPost: async (req, res) => {
-        const db = req.app.get('db')
+  createSubforumPost: async (req, res) => {
+    const db = req.app.get("db");
 
-        const post_time = moment().format('LT');
-        // const {post_author_id} = req.session.user
-        const {subforumId} = req.params
-        const {post_title, post_content, post_type_id, post_author_id} = req.body
+    const post_time = moment().format("LT");
+    // const {post_author_id} = req.session.user
+    const { subforumId } = req.params;
+    const { post_title, post_content, post_type_id, post_author_id } = req.body;
 
-        let post = await db.post.create_subforum_post(post_title, post_content, post_author_id, subforumId, post_type_id, post_time)
+    let post = await db.post.create_subforum_post(
+      post_title,
+      post_content,
+      post_author_id,
+      subforumId,
+      post_type_id,
+      post_time
+    );
 
-        res.status(200).send(post)
-    },
+    res.status(200).send(post);
+  },
 
-    getSingleSubforumPost: async (req, res) => {
-        const db = req.app.get('db')
+  getSingleSubforumPost: async (req, res) => {
+    const db = req.app.get("db");
 
-        const {subforumId, postId} = req.params
+    const { subforumId, postId } = req.params;
 
-        let post = await db.post.get_single_post(subforumId, postId)
+    let post = await db.post.get_single_post(subforumId, postId);
 
-        res.status(200).send(post)
-    },
+    res.status(200).send(post);
+  },
 
-    updateSubforumPost: async (req, res) => {
-        const db = req.app.get('db')
+  updateSubforumPost: async (req, res) => {
+    const db = req.app.get("db");
 
-        const {subforumId, postId} = req.params
-        const {post_title, post_content} = req.body
+    const { subforumId, postId } = req.params;
+    const { post_title, post_content } = req.body;
 
-        let udpatedPost = await db.post.update_post(postId, subforumId, post_title, post_content)
+    let udpatedPost = await db.post.update_post(
+      postId,
+      subforumId,
+      post_title,
+      post_content
+    );
 
-        res.status(200).send(udpatedPost)
-    },
+    res.status(200).send(udpatedPost);
+  },
 
-    deleteSubforumPost: (req, res) => {
-        const db = req.app.get('db')
+  deleteSubforumPost: (req, res) => {
+    const db = req.app.get("db");
 
-        const {postId} = req.params
+    const { postId } = req.params;
 
-        db.post.delete_post(postId)
+    db.post.delete_post(postId);
 
-        res.sendStatus(200)
-    }
-}
+    res.sendStatus(200);
+  },
+  getAllPostsNoUser: async (req, res) => {
+    const db = req.app.get("db");
+
+    const allPosts = await db.post.get_all_posts_no_user();
+    res.status(200).send(allPosts);
+  },
+};
--- server/userController.js
+++ server/userController.js
@@ -1,21 +1,36 @@
 module.exports = {
-    getAllUsers: async (req, res) => {
-        const db = req.app.get('db')
+  getAllUsers: async (req, res) => {
+    const db = req.app.get("db");
 
-        const users = await db.all_users()
+    const users = await db.all_users();
 
-        res.status(200).send(users)
-    },
+    res.status(200).send(users);
+  },
 
-    getUser: async (req, res) => {
-        const db = req.app.get('db')
+  getUser: async (req, res) => {
+    const db = req.app.get("db");
 
-        const {userId} = req.params
+    const { userId } = req.params;
 
-        const user = await db.user(userId)
+    const user = await db.user(userId);
 
-        res.status(200).send(user)
+    res.status(200).send(user);
+  },
+  updateUser: async (req, res) => {
+    const db = req.app.get("db");
+    const { userId } = req.params;
+    const { user_id } = req.session.user;
+    const { username, user_email, user_image, user_banner } = req.body;
+    const updatedUser = await db.user.update_user(
+      userId,
+      username,
+      user_email,
+      user_image,
+      user_banner
+    );
+    if (userId === user_id) {
+      return res.status(200).send(updatedUser);
     }
-
-
-}
+    return res.sendStatus(200);
+  },
+};
