dbdf7322ad6d95b76935c45f40c2eddae882cbfa
tjlemperle <tjlemperle@gmail.com>
2020-07-13 11:52:17 -0600

    db file structure

--- server/postController.js
+++ server/postController.js
@@ -1,1 +1,12 @@
 //includes comments
+const moment = require('moment');
+
+module.exports = {
+    getAllSubforumPosts: async (req, res) => {
+        const db = req.app.get('db')
+
+        const {subforumId} = req.params
+
+        let post = db.
+    }
+}
